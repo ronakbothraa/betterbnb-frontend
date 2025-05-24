cc9b0c8736a8cf51f4da4816f73edef7b03f0a4c
Ronak Bothra <ronakbothraa@gmail.com>
2025-05-24 18:44:17 +0530

    Install nextjs and setup basic stuff

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      
+      <h2 className="text-airbnb">
+        Better Bnb
+      </h2>
+    </main>  
+  );
+}
