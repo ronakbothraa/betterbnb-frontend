d0886161a42b7475bfd832275cf83c7a09e5ec15
Ronak Bothra <ronakbothraa@gmail.com>
2025-05-25 17:42:06 +0530

    ui design: dropdown manu user profile

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,14 +1,42 @@
-import { CircleUserRound, Menu } from "lucide-react"
+"use client";
+
+import { CircleUserRound, Menu } from "lucide-react";
+import { useState } from "react";
+import MenuLink from "./MenuLink";
 
 const UserNav = () => {
-  return (
-    <div className="p-2 relative inline-block border rounded-full">
-        <button className="flex items-center">
-            <Menu />
-            <CircleUserRound />
-        </button>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default UserNav
+  return (
+    <div className="relative inline-block border rounded-full">
+      <div
+        className={`${
+          isOpen && "bg-airbnb"
+        } p-2 rounded-full hover:bg-airbnb transition`}
+      >
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center"
+        >
+          <Menu color={isOpen ? "white" : "black"} />
+          <CircleUserRound color={isOpen ? "white" : "black"} />
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="cursor-pointer flex flex-col w-[220px] absolute top-[50px] right-0 bg-white border border-gray-300 rounded-xl shadow-md">
+          <MenuLink
+            label="Register"
+            onClick={() => console.log("menu link clicked")}
+          />
+          <MenuLink
+            label="Login"
+            onClick={() => console.log("menu link clicked")}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserNav;
