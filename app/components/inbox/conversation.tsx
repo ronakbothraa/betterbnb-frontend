40ce2da0cd3c176e70d24c162e1a69e2cc166ecc
Ronak Bothra <ronakbothraa@gmail.com>
2025-06-04 17:06:52 +0530

    feat: fetching data from backend chat api to show all the chats in the inbox page

--- app/components/inbox/conversation.tsx
+++ app/components/inbox/conversation.tsx
@@ -1,11 +1,29 @@
-const Conversation = () => {
-  return (
-    <div className="px-6 py-4 border cursor-pointer border-gray-300 rounded-xl">
-        <p className="mb-6 text-xl">John Doe</p>
+"use client";
 
-        <p className="text-airbnb-dark ">Go to conversation</p>
-    </div>
-  )
+import { ConversationType } from "@/app/inbox/page";
+import { useRouter } from "next/navigation";
+
+interface ConversationProps {
+  conversation: ConversationType;
+  user: string;
 }
 
-export default Conversation
+const Conversation: React.FC<ConversationProps> = ({ conversation, user }) => {
+  const router = useRouter();
+  const otherUser = conversation.user.find((u) => u.id !== user);
+
+  return (
+    <div className="px-6 py-4 border cursor-pointer border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">{otherUser?.name}</p>
+
+      <p
+        onClick={() => router.push(`/inbox/${conversation.id}`)}
+        className="text-airbnb-dark "
+      >
+        Go to conversation
+      </p>
+    </div>
+  );
+};
+
+export default Conversation;
--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,15 +1,46 @@
-import Conversation from "../components/inbox/conversation"
+import Conversation from "../components/inbox/conversation";
+import { getAuthCookies } from "../lib/actions";
+import apiService from "../services/apiService";
+import useLoginModal from "../hooks/useLoginModal";
 
-const InboxPage = () => {
-  return (
-    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-2">
-        <h1 className="my-6 text-2xl">Inbox</h1>
-        
-        <Conversation />
-        <Conversation />
-        <Conversation />
-    </main>
-  )
+export type UserType = {
+  id: string;
+  name: string;
+  avatar_url: string;
+};
+
+export type ConversationType = {
+  id: string;
+  user: UserType[];
 }
 
-export default InboxPage
+const InboxPage = async () => {
+  const user = await getAuthCookies();
+
+  if (!user) {
+    return (
+      <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-2">
+        <h1 className="my-6 text-2xl">Inbox</h1>
+        <p>
+          Please <strong className="text-airbnb">log in</strong> to view your
+          inbox.
+        </p>
+      </main>
+    );
+  }
+
+  const conversations: ConversationType[] = await apiService.get("api/chat/");
+
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-2">
+      <h1 className="my-6 text-2xl">Inbox</h1>
+      {
+        conversations.map((conversation: ConversationType) => (
+          <Conversation key={conversation.user[0].id} user={user} conversation={conversation}/>
+        ))
+      }
+    </main>
+  );
+};
+
+export default InboxPage;
